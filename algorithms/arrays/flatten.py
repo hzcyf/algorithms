193e02057f8bea5ff9144739aa1568a50dec9cc5
hzc <1591563365@qq.com>
2018-08-03 11:06:18 +0800

    Update flatten.py

--- algorithms/arrays/flatten.py
+++ algorithms/arrays/flatten.py
@@ -2,6 +2,7 @@
 Implement Flatten Arrays.
 Given an array that may contain nested arrays,
 produce a single resultant array.
+for example: input:[1, [2, 3], [4, [5, 6], 7], 8], output: [1, 2, 3, 4, 5, 6, 7, 8]
 """
 from collections import Iterable
 
